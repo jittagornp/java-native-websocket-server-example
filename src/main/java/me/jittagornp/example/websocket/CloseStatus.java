a44d3f43e41c5fe5c70210b799ddf01dd470fb6d
jittagornpitakmetagoon <jittagornp@gmail.com>
2021-01-23 15:11:34 +0700

    Implement close status

---
+++ src/main/java/me/jittagornp/example/websocket/CloseStatus.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021-Current jittagornp.me
+ */
+package me.jittagornp.example.websocket;
+
+/**
+ * https://tools.ietf.org/html/rfc6455#section-7.4.1
+ *
+ * @author jitta
+ */
+public enum ConnectionClose {
+
+    NORMAL(1000, "Normal close"),
+    GOING_AWAY(10001, "Going away"),
+    PROTOCOL_ERROR(1002, "Protocol error"),
+    REFUSE(1003, "Refuse"),
+    NO_STATUS_CODE(1005, "No status code"),
+    ABNORMAL_CLOSE(1006, "Abnormal close"),
+    NON_UTF8(1007, "Non UTF-8"),
+    POLICY_VALIDATION(1008, "Policy validation"),
+    TOO_BIG(1009, "Too big"),
+    EXTENSION(1010, "Extension"),
+    UNEXPECTED_CONDITION(1011, "Unexpected condition"),
+    SERVICE_RESTART(1012, "Service restart"),
+    TRY_AGAIN_LATER(1013, "Try again later"),
+    BAD_GATEWAY(1014, "Bad gateway"),
+    TLS_ERROR(1015, "TLS error");
+
+    private final int statusCode;
+    private final String reason;
+
+    private ConnectionClose(final int statusCode, final String reason) {
+        this.statusCode = statusCode;
+        this.reason = reason;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public static ConnectionClose fromStatusCode(final int statusCode) {
+        final ConnectionClose[] connectionCloses = values();
+        for (ConnectionClose connectionClose : connectionCloses) {
+            if (connectionClose.getStatusCode() == statusCode) {
+                return connectionClose;
+            }
+        }
+        throw new UnsupportedOperationException("Unknown status code");
+    }
+}
